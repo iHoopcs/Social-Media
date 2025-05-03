0fa76b632623493ec5e9bcaa4e924e981eaf2e69
Caleb Simmons <97637707+iHoopcs@users.noreply.github.com>
2025-05-03 18:16:50 -0400

    POST createUser logic fix

--- my-app/models/user.ts
+++ my-app/models/user.ts
@@ -10,8 +10,10 @@ interface User {
 const UserSchema = new Schema<User>({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
 
-export const User = mongoose.model("User", UserSchema);
+const User = mongoose.models.User || mongoose.model("User", UserSchema);
+
+export default User;
--- my-app/src/app/api/(auth)/register/route.ts
+++ my-app/src/app/api/(auth)/register/route.ts
@@ -1,28 +1,44 @@
 import { connectMongoDB } from "./../../../../lib/mongodb";
-import { User } from "../../../../../models/user";
+import User from "../../../../../models/user";
 import bcrypt from "bcrypt";
 
 export async function POST(request: Request) {
-  await connectMongoDB();
-  //fetch form data
   const { payload } = await request.json();
   if (!payload) return new Error("Missing payload data");
-  console.log(payload);
 
-  //hash password
-  const hashedPassword = await bcrypt.hash(payload.password, 10);
-  //   construct & save user
-  await User.create({
-    firstName: payload.firstName,
-    lastName: payload.lastName,
-    email: payload.email,
-    password: hashedPassword,
-  });
+  try {
+    await connectMongoDB();
 
-  return new Response(JSON.stringify("User created!"), {
-    status: 201,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+    const foundUser = await User.findOne({ email: payload.email });
+    if (foundUser) {
+      return new Response(
+        JSON.stringify("Account already exists with provided email"),
+        {
+          status: 400,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    //hash password
+    const hashedPassword = await bcrypt.hash(payload.password, 10);
+
+    await User.create({
+      firstName: payload.firstName,
+      lastName: payload.lastName,
+      email: payload.email,
+      password: hashedPassword,
+    });
+
+    return new Response(JSON.stringify("User created!"), {
+      status: 201,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
