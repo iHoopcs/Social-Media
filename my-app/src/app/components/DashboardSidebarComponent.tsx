57ffe60be2f0d3d5684834f0a774da6f7275bd89
Caleb Simmons <csimmons718@bears.shawu.edu>
2025-05-06 15:28:30 -0400

    dashboard - sidebar progress

---
+++ my-app/src/app/components/DashboardSidebarComponent.tsx
@@ -0,0 +1,122 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import {
+  Calendar,
+  ChevronUp,
+  Home,
+  Inbox,
+  Search,
+  Settings,
+} from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { getUserFromSession } from "@/lib/session-auth";
+import { redirect } from "next/navigation";
+
+// Menu items.
+const items = [
+  {
+    title: "Home",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Inbox",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "Calendar",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "Search",
+    url: "#",
+    icon: Search,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+];
+
+export const DashboardSidebar = async () => {
+  const user = await getUserFromSession();
+  if (!user) redirect("/");
+
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem className="text-center">
+            <Button className="cursor-pointer mb-5 w-full max-w-sm">
+              New Post?
+            </Button>
+          </SidebarMenuItem>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton className="cursor-pointer">
+                  <span className="text-lg font-sans">
+                    {user.firstName} {user.lastName}
+                  </span>
+                  <ChevronUp className="ml-auto" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                side="top"
+                className="w-[--radix-popper-anchor-width]"
+              >
+                <DropdownMenuItem className="cursor-pointer">
+                  <span>Account</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem className="cursor-pointer">
+                  <span>Logout</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+  );
+};
--- my-app/src/app/pages/dashboard/layout.tsx
+++ my-app/src/app/pages/dashboard/layout.tsx
@@ -1,4 +1,5 @@
-"use client";
+
+
 import { handleLogout } from "@/app/components/LogoutComponent";
 import { IoIosLogOut } from "react-icons/io";
 import {
@@ -18,6 +19,8 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { DashboardSidebar } from "@/app/components/DashboardSidebarComponent";
 
 export default function DashboardLayout({
   children,
@@ -26,49 +29,56 @@ export default function DashboardLayout({
 }) {
   return (
     <div className="flex flex-col min-h-screen">
-      <header className="bg-slate-900 text-white p-4 flex flex-row items-center justify-between">
-        <span className="text-2xl font-bold">Social Media</span>
-        <span>
-          <TooltipProvider>
-            <Tooltip>
-              <AlertDialog>
-                <TooltipTrigger asChild>
-                  <AlertDialogTrigger asChild>
-                    <IoIosLogOut
-                      size={30}
-                      className="hover:text-red-600 cursor-pointer"
-                    />
-                  </AlertDialogTrigger>
-                </TooltipTrigger>
-                <TooltipContent>
-                  <p className="text-sm">Logout?</p>
-                </TooltipContent>
-                <AlertDialogContent>
-                  <AlertDialogHeader>
-                    <AlertDialogTitle>Are you sure?</AlertDialogTitle>
-                    <AlertDialogDescription>
-                      You will be logged out and prompted to log back in.
-                    </AlertDialogDescription>
-                  </AlertDialogHeader>
-                  <AlertDialogFooter>
-                    <AlertDialogCancel className="cursor-pointer">
-                      Cancel
-                    </AlertDialogCancel>
-                    <AlertDialogAction
-                      className="cursor-pointer"
-                      onClick={handleLogout}
-                    >
-                      Logout
-                    </AlertDialogAction>
-                  </AlertDialogFooter>
-                </AlertDialogContent>
-              </AlertDialog>
-            </Tooltip>
-          </TooltipProvider>
-        </span>
-      </header>
+      <SidebarProvider defaultOpen>
+        <div className="flex flex-row items-baseline">
+          <DashboardSidebar />
+          <SidebarTrigger size={"lg"} className="cursor-pointer" />
 
-      <main className="flex-1">{children}</main>
+          <main className="flex-1 p-4">{children}</main>
+        </div>
+      </SidebarProvider>
     </div>
   );
+}
+
+// Logout functionality
+{
+  /* <span>
+  <TooltipProvider>
+    <Tooltip>
+      <AlertDialog>
+        <TooltipTrigger asChild>
+          <AlertDialogTrigger asChild>
+            <IoIosLogOut
+              size={30}
+              className="hover:text-red-600 cursor-pointer"
+            />
+          </AlertDialogTrigger>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p className="text-sm">Logout?</p>
+        </TooltipContent>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              You will be logged out and prompted to log back in.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel className="cursor-pointer">
+              Cancel
+            </AlertDialogCancel>
+            <AlertDialogAction
+              className="cursor-pointer"
+              onClick={handleLogout}
+            >
+              Logout
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </Tooltip>
+  </TooltipProvider>
+</span>; */
 }
